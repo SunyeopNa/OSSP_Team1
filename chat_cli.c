029ba01c57edca5f0d6d602fd354dd71a6c92c67
coinnip <coinnip@gmail.com>
2017-06-20 01:14:00 +0900

    Add files via upload

--- chat_cli.c
+++ chat_cli.c
@@ -1,3 +1,6 @@
+Ôªø#include <glib.h>
+#include <gdk/gdk.h>
+#include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,89 +12,511 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define MAXLINE     1000
-#define NAME_LEN    20
+#define SERVER_PORT 2402
+#define SERVER_IP "127.0.0.1"
+#define MAX_LINE 1024
 
-char *EXIT_STRING= "exit";
-// º“ƒœ ª˝º∫ π◊ º≠πˆ ø¨∞·, ª˝º∫µ» º“ƒœ∏Æ≈œ
+char buf[MAX_LINE];
+
+int pic_array[400][400][3];
+char *EXIT_STRING = "exit";
+char *START_STRING = "OSSP Í∑∏Î¶ºÏ±ÑÌåÖ\n\0";
+char *MAIN_MENU = "\n\tÏÑúÎπÑÏä§ Î≤àÌò∏Î•º ÏÑ†ÌÉùÌïòÏÑ∏Ïöî.\n 1.Î∞© Í∞úÏÑ§ \n 2.Î∞© Ï∞∏Ïó¨\n\n\n\n<Î≤àÌò∏/Î™ÖÎ†π(go,exit)>\n\0 ";
+char *CREAT_ROOM = "enter chatting room subject ";
+
+fd_set read_fds;
+int first_time = 0;
+int menu_num = 0;
+int  s;
+int whats_number = 0;
+static void chogiwha(GtkWidget *widget, int whts);
+/* Backing pixmap for drawing area */
+static GdkPixmap *pixmap = NULL;
+
+GtkWidget *colorseldlg = NULL;
+GtkWidget *color_select_area = NULL;
+
+GdkColor user_color; // color
+GdkColor rec_color;
+int user_brush_size = 10;
+static void color_changed_cb(GtkWidget *widget,
+	GtkColorSelection *colorsel)
+{
+	GdkColor ncolor;
+
+	gtk_color_selection_get_current_color(colorsel, &ncolor);
+
+	gtk_widget_modify_bg(color_select_area, GTK_STATE_NORMAL, &ncolor);
+}
+
+static gboolean area_event(GtkWidget *widget,
+	GdkEvent *event,
+	gpointer client_data)
+{
+	gint handled = FALSE;
+	gint response;
+	GtkColorSelection *colorsel;
+
+	if (event->type == GDK_BUTTON_PRESS)
+	{
+		handled = TRUE;
+
+		if (colorseldlg == NULL)
+		{
+			colorseldlg = gtk_color_selection_dialog_new("Select background color");
+
+		}
+		colorsel = GTK_COLOR_SELECTION(GTK_COLOR_SELECTION_DIALOG(colorseldlg)->colorsel);
+
+		gtk_color_selection_set_previous_color(colorsel, &user_color);
+		gtk_color_selection_set_current_color(colorsel, &user_color);
+		gtk_color_selection_set_has_palette(colorsel, TRUE);
+
+		g_signal_connect(colorsel, "color_changed",
+			G_CALLBACK(color_changed_cb), (gpointer)colorsel);
+		response = gtk_dialog_run(GTK_DIALOG(colorseldlg));
+
+		if (response == GTK_RESPONSE_OK)
+		{
+			gtk_color_selection_get_current_color(colorsel, &user_color);
+
+		}
+		else
+		{
+			gtk_widget_modify_bg(color_select_area, GTK_STATE_NORMAL, &user_color);
+
+		}
+
+		gtk_widget_hide(colorseldlg);
+	}
+	return handled;
+}
+
+static gboolean destroy_window(GtkWidget *widget, GdkEvent *event, gpointer client_data)
+{
+	gtk_main_quit();
+	return TRUE;
+}
+/* Create a new backing pixmap of the appropriate size */
+static gint configure_event(GtkWidget         *widget,
+	GdkEventConfigure *event)
+{
+	if (pixmap)
+		gdk_pixmap_unref(pixmap);
+
+	pixmap = gdk_pixmap_new(widget->window,
+		widget->allocation.width,
+		widget->allocation.height,
+		-1);
+	gdk_draw_rectangle(pixmap,
+		widget->style->white_gc,
+		TRUE,
+		0, 0,
+		widget->allocation.width,
+		widget->allocation.height);
+
+	if (first_time == 0)
+	{
+		first_time++;
+		chogiwha(widget, whats_number);
+		printf("ÌÉàÏ∂ú");
+	}
+	return TRUE;
+}
+/* Redraw the screen from the backing pixmap */
+static gint expose_event(GtkWidget      *widget,
+	GdkEventExpose *event)
+{
+	gdk_draw_pixmap(widget->window,
+		widget->style->bg_gc[GTK_WIDGET_STATE(widget)],
+		pixmap,
+		event->area.x, event->area.y,
+		event->area.x, event->area.y,
+		event->area.width, event->area.height);
+	return FALSE;
+}
+/* Draw a rectangle on the screen */
+int coor_x, coor_y;
+char bufall_recv[40];
+static void chogiwha(GtkWidget *widget, int whts)
+{
+	char test[29];
+	char * ptr;
+	int col_pix1;
+	GdkRectangle user_update_rect;
+	if (whats_number != 0)
+	{
+		recv(s, test, 29, 0);
+		while (coor_x != 399 || coor_y != 399)
+		{
+			recv(s, test, 29, 0);
+			ptr = strtok(test, " ");
+			coor_x = atoi(ptr);
+			ptr = strtok(NULL, " ");
+			coor_y = atoi(ptr);
+			ptr = strtok(NULL, " ");
+			rec_color.red = atoi(ptr);
+			ptr = strtok(NULL, " ");
+			rec_color.green = atoi(ptr);
+			ptr = strtok(NULL, " ");
+			rec_color.blue = atoi(ptr);
+			ptr = strtok(NULL, " ");
+			col_pix1 = atoi(ptr);
+
+			user_update_rect.x = coor_x;
+			user_update_rect.y = coor_y;
+			user_update_rect.width = 1;
+			user_update_rect.height = 1;
+			if (rec_color.red != 0 || rec_color.green != 0 || rec_color.blue != 0)
+			{
+				gdk_gc_set_rgb_fg_color(widget->style->bg_gc[0], &rec_color);
+				gdk_draw_rectangle(pixmap,
+					widget->style->bg_gc[0],
+					TRUE,
+					user_update_rect.x, user_update_rect.y,
+					user_update_rect.width, user_update_rect.height);
+				gtk_widget_draw(widget, &user_update_rect);
+			}
+		}
+	}
+}
+static void draw_brush(GtkWidget *widget, gdouble x, gdouble y)
+{
+	GdkRectangle update_rect;
+	GdkRectangle user_update_rect;
+
+	char string_x[4];
+	char string_y[4];
+	char col_red[6];
+	char col_green[6];
+	char col_blue[6];
+	char col_pix[3];
+	char bufall[40];
+
+
+	sprintf(string_x, "%d", (int)x);
+	sprintf(string_y, "%d", (int)y);
+	sprintf(col_red, "%d", user_color.red);
+	sprintf(col_green, "%d", user_color.green);
+	sprintf(col_blue, "%d", user_color.blue);
+	sprintf(col_pix, "%d", user_brush_size);
+
+	sprintf(bufall, "%s %s %s %s %s %s\0", string_x, string_y, col_red, col_green, col_blue, col_pix);
+	printf("[send]%s\n", bufall);
+	send(s, bufall, 40, 0);
+
+	int col_red1;
+	int col_green1;
+	int col_blue1;
+	int col_pix1;
+	char *ptr;
+
+	recv(s, bufall_recv, 40, 0);
+	bufall_recv[30] = '\0';
+	printf("[recv]%s\n", bufall_recv);
+
+	ptr = strtok(bufall_recv, " ");
+	coor_x = atoi(ptr);
+	ptr = strtok(NULL, " ");
+	coor_y = atoi(ptr);
+	ptr = strtok(NULL, " ");
+	rec_color.red = atoi(ptr);
+	ptr = strtok(NULL, " ");
+	rec_color.green = atoi(ptr);
+	ptr = strtok(NULL, " ");
+	rec_color.blue = atoi(ptr);
+	ptr = strtok(NULL, " ");
+	col_pix1 = atoi(ptr);
+
+	gdk_gc_set_rgb_fg_color(widget->style->bg_gc[0], &rec_color);
+	gdk_gc_set_rgb_fg_color(widget->style->fg_gc[0], &user_color);
+
+	user_update_rect.x = coor_x - 5;
+	user_update_rect.y = coor_y - 5;
+	user_update_rect.width = col_pix1;
+	user_update_rect.height = col_pix1;
+
+	update_rect.x = (int)x - 5;
+	update_rect.y = (int)y - 5;
+	update_rect.height = user_brush_size;
+	update_rect.width = user_brush_size;
+	gdk_draw_rectangle(pixmap,
+		widget->style->bg_gc[0],
+		TRUE,
+		user_update_rect.x, user_update_rect.y,
+		user_update_rect.width, user_update_rect.height);
+	gtk_widget_draw(widget, &user_update_rect);
+
+	gdk_draw_rectangle(pixmap,
+		widget->style->fg_gc[0],
+		TRUE,
+		update_rect.x, update_rect.y,
+		update_rect.width, update_rect.height);
+	gtk_widget_draw(widget, &update_rect);
+}
+static gint button_press_event(GtkWidget *widget, GdkEventButton *event)
+{
+
+	if (event->button == 1 && pixmap != NULL)
+	{
+
+		if ((int)(event->x - 0) < 0) event->x = 0;
+		if ((int)(event->x - 0) >= 400) event->x = 400;
+		if ((int)(event->y - 0) < 0) event->y = 0;
+		if ((int)(event->y - 0) >= 400) event->y = 400;
+		//0Ïùò ÏòÅÏó≠
+		if (whats_number == 0)
+		{
+			if ((event->x - 200) <= 0 && (event->y - 200) <= 0)
+				draw_brush(widget, event->x, event->y);
+		}
+		//1ÏùòÏòÅÏó≠
+		else if (whats_number == 1)
+		{
+			if ((event->x - 200) >= 0 && (event->y - 200) <= 0)
+				draw_brush(widget, event->x, event->y);
+		}
+		//2Ïùò ÏòÅÏó≠
+		else if (whats_number == 2)
+		{
+			if ((event->x - 200) <= 0 && (event->y - 200) >= 0)
+				draw_brush(widget, event->x, event->y);
+		}
+		//3Ïùò ÏòÅÏó≠
+		else if (whats_number == 3)
+		{
+			if ((event->x - 200) >= 0 && (event->y - 200) >= 0)
+				draw_brush(widget, event->x, event->y);
+		}
+	}
+	return TRUE;
+}
+
+static gint motion_notify_event(GtkWidget *widget, GdkEventMotion *event)
+{
+	int x, y;
+	GdkModifierType state;
+	if (event->is_hint)
+		gdk_window_get_pointer(event->window, &x, &y, &state);
+	else
+	{
+		x = event->x;
+		y = event->y;
+		state = event->state;
+	}
+	if (state & GDK_BUTTON1_MASK && pixmap != NULL)
+	{
+		if ((int)(event->x - 0) < 0) event->x = 0;
+		if ((int)(event->x - 0) >= 400) event->x = 399;
+		if ((int)(event->y - 0) < 0) event->y = 0;
+		if ((int)(event->y - 0) >= 400) event->y = 399;
+		//0Ïùò ÏòÅÏó≠
+		if (whats_number == 0)
+		{
+			if ((event->x - 200) <= 0 && (event->y - 200) <= 0)
+				draw_brush(widget, event->x, event->y);
+		}
+		//1ÏùòÏòÅÏó≠
+		else if (whats_number == 1)
+		{
+			if ((event->x - 200) >= 0 && (event->y - 200) <= 0)
+				draw_brush(widget, event->x, event->y);
+		}
+		//2Ïùò ÏòÅÏó≠
+		else if (whats_number == 2)
+		{
+			if ((event->x - 200) <= 0 && (event->y - 200) >= 0)
+				draw_brush(widget, event->x, event->y);
+		}
+		//3Ïùò ÏòÅÏó≠
+		else if (whats_number == 3)
+		{
+			if ((event->x - 200) >= 0 && (event->y - 200) >= 0)
+				draw_brush(widget, event->x, event->y);
+		}
+	}
+	return TRUE;
+}
+
+void quit()
+{
+	send(s, "exit", 5, 0);
+	gtk_exit(0);
+}
 int tcp_connect(int af, char *servip, unsigned short port);
 void errquit(char *mesg) { perror(mesg); exit(1); }
+int k = 0;
+int main()
+{
+	int maxfdp1;
+	int select_num;
 
-int main(int argc, char *argv[]) {
-	char bufall[MAXLINE+NAME_LEN], // ¿Ã∏ß+∏ﬁΩ√¡ˆ∏¶ ¿ß«— πˆ∆€
-	     *bufmsg;  // bufall ø°º≠ ∏ﬁΩ√¡ˆ∫Œ∫–¿« ∆˜¿Œ≈Õ
-	int maxfdp1, s, namelen;   // √÷¥Î º“ƒœπ¯»£, º“ƒœ µΩ∫≈©∏≥≈Õ, ¿Ã∏ß±Ê¿Ã
-	fd_set read_fds;
+	int len = strlen(START_STRING);
+	int len2 = strlen(MAIN_MENU);
 
-	if(argc != 4) {
-		printf("ªÁøÎπ˝ : %s sever_ip  port name \n", argv[0]);
-		exit(0);
-	}
 
-	sprintf(bufall, "[%s] :", argv[3]);  // bufall ¿« æ’∫Œ∫–ø° ¿Ã∏ß¿ª ¿˙¿Â
-	namelen= strlen(bufall);
-	bufmsg = bufall+namelen;  // ∏ﬁΩ√¡ˆ Ω√¿€ ∫Œ∫– ¡ˆ¡§
+	char *ptr;
+	char bufall[MAX_LINE];
+	char first_buf[40];
+	char bufmsg[MAX_LINE];
+	char recvmsg[10240];
 
-	s = tcp_connect(AF_INET, argv[1], atoi(argv[2]));
-
-	if(s==-1)
+	s = tcp_connect(AF_INET, SERVER_IP, SERVER_PORT);
+	if (s == -1)
 		errquit("tcp_connect fail");
-
-	system("clear");
-	puts("º≠πˆø° ¡¢º” ¡ﬂ ¿‘¥œ¥Ÿ.......");
 
 	maxfdp1 = s + 1;
 	FD_ZERO(&read_fds);
 
-	while(1) {
+	recvmsg[0] = '\0';
+	while (strstr(recvmsg, "X") == NULL)
+	{
 		FD_SET(0, &read_fds);
-	        FD_SET(s, &read_fds);
-        	
-		if(select(maxfdp1, &read_fds, NULL,NULL,NULL) < 0)
-            		errquit("select fail");
-		
-		if (FD_ISSET(s, &read_fds)) {
-			int nbyte;
-			
-			if ((nbyte = recv(s, bufmsg, MAXLINE, 0)) > 0)  {
-				bufmsg[nbyte] = 0;
-				printf("%s \n", bufmsg);
-//				printf("<π¯»£/∏Ì∑…(go,exit)>: ");
-			}
-		}//end if
-	
-	        if (FD_ISSET(0, &read_fds)) {
-        		if(fgets(bufmsg, MAXLINE, stdin)) {
- 
-	               		if (send(s, bufall, namelen+strlen(bufmsg), 0) < 0)
-        				puts("Error : Write error on socket.");
+		FD_SET(s, &read_fds);
 
-               			if (strstr(bufmsg, EXIT_STRING) != NULL ) {
+		if (select(maxfdp1, &read_fds, NULL, NULL, NULL) < 0)
+			errquit("select fail");
+
+		if (FD_ISSET(s, &read_fds))
+		{
+			int nbyte = recv(s, recvmsg, 10240, 0);
+			if (strstr(recvmsg, "OSSP") != NULL)
+				recvmsg[len + len2] = '\0';
+			//XÍ∞Ä Ìè¨Ìï® ÎêòÏñ¥ ÏûàÏúºÎ©¥ Î∞îÎ°ú whlieÎ¨∏ÏùÑ Îπ†Ï†∏ÎÇòÍ∞ê.
+			if (strstr(recvmsg, "X"))
+			{
+				break;
+			}
+			recvmsg[len + len2] = '\0';
+			if (nbyte>0)
+			{
+				printf("%s\n", recvmsg);
+			}
+		}
+		if (FD_ISSET(0, &read_fds))
+		{
+			if (fgets(bufmsg, 40, stdin))
+			{
+				if (send(s, bufmsg, 40, 0)<0)
+					puts("Error : write error on socket.");
+				if (strstr(bufmsg, EXIT_STRING) != NULL)
+				{
 					puts("Good bye.");
 					close(s);
 					exit(0);
-				}//end if
-			}//end if
-		} //end if
-	} // end of while
+				}
+			}
+		}
+	}
+	recvmsg[4] = '\0';
+	ptr = strtok(recvmsg, " ");
+	ptr = strtok(NULL, " ");
+	whats_number = atoi(ptr);
+	printf("<whats_number>%d\n", whats_number);
+	recvmsg[0] = '\0';
+
+	GtkWidget *window1;
+	GtkWidget *window2;
+	GtkWidget *drawing_area;
+	GtkWidget *vbox;
+	GtkWidget *button;
+
+	gtk_init(NULL, NULL);
+	window1 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	window2 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_widget_set_name(window1, "Test Input");
+
+	gtk_window_set_title(GTK_WINDOW(window2), "Color Selection");
+	gtk_window_set_policy(GTK_WINDOW(window2), TRUE, TRUE, TRUE);
+
+	g_signal_connect(window2, "delete-event", G_CALLBACK(destroy_window), (gpointer)window2);
+
+	color_select_area = gtk_drawing_area_new();
+
+	user_color.red = 0;
+	user_color.blue = 65535;
+	user_color.green = 0;
+
+	gtk_widget_modify_bg(color_select_area, GTK_STATE_NORMAL, &user_color);
+	gtk_widget_set_size_request(GTK_WIDGET(color_select_area), 200, 200);
+	gtk_widget_set_events(color_select_area, GDK_BUTTON_PRESS_MASK);
+
+	g_signal_connect(GTK_OBJECT(color_select_area), "event", GTK_SIGNAL_FUNC(area_event), (gpointer)color_select_area);
+
+	gtk_container_add(GTK_CONTAINER(window2), color_select_area);
+
+
+	vbox = gtk_vbox_new(FALSE, 0);
+	gtk_container_add(GTK_CONTAINER(window1), vbox);
+	gtk_widget_show(vbox);
+
+	gtk_signal_connect(GTK_OBJECT(window1), "destroy",
+		GTK_SIGNAL_FUNC(quit), NULL);
+
+	/* Create the drawing area */
+
+	drawing_area = gtk_drawing_area_new();
+	gtk_drawing_area_size(GTK_DRAWING_AREA(drawing_area), 400, 400);
+	gtk_box_pack_start(GTK_BOX(vbox), drawing_area, TRUE, TRUE, 0);
+
+	gtk_widget_show(drawing_area);
+
+	/* Signals used to handle backing pixmap */
+
+	gtk_signal_connect(GTK_OBJECT(drawing_area), "expose_event",
+		(GtkSignalFunc)expose_event, NULL);
+	gtk_signal_connect(GTK_OBJECT(drawing_area), "configure_event",
+		(GtkSignalFunc)configure_event, NULL);
+
+	/* Event signals */
+
+	gtk_signal_connect(GTK_OBJECT(drawing_area), "motion_notify_event",
+		(GtkSignalFunc)motion_notify_event, NULL);
+	gtk_signal_connect(GTK_OBJECT(drawing_area), "button_press_event",
+		(GtkSignalFunc)button_press_event, NULL);
+
+	gtk_widget_set_events(drawing_area, GDK_EXPOSURE_MASK
+		| GDK_LEAVE_NOTIFY_MASK
+		| GDK_BUTTON_PRESS_MASK
+		| GDK_POINTER_MOTION_MASK
+		| GDK_POINTER_MOTION_HINT_MASK);
+
+	/* .. And a quit button */
+	button = gtk_button_new_with_label("Quit");
+	gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
+
+	gtk_signal_connect_object(GTK_OBJECT(button), "clicked",
+		GTK_SIGNAL_FUNC(gtk_widget_destroy),
+		GTK_OBJECT(window1));
+	gtk_widget_show(button);
+
+	gtk_widget_show(window1);
+
+	gtk_widget_show(color_select_area);
+	gtk_widget_show(window2);
+
+	char test[29];
+	int c_x = 0;
+	int c_y = 0;
+	int col_pix1;
+	int k = 0;
+
+	GdkRectangle user_update_rect;
+
+	gtk_main();
+	return 0;
 }
-
-int tcp_connect(int af, char *servip, unsigned short port) {
+int tcp_connect(int af, char *servip, unsigned short port)
+{
 	struct sockaddr_in servaddr;
-	int  s;
-    	// º“ƒœ ª˝º∫
-	if ((s = socket(af, SOCK_STREAM, 0)) < 0)
-    		return -1;
-    	// √§∆√ º≠πˆ¿« º“ƒœ¡÷º“ ±∏¡∂√º servaddr √ ±‚»≠
-    	bzero((char *)&servaddr, sizeof(servaddr));
-    	servaddr.sin_family = af;
-    	inet_pton(AF_INET, servip, &servaddr.sin_addr);
-    	servaddr.sin_port = htons(port);
 
-	// ø¨∞·ø‰√ª
-	if(connect(s, (struct sockaddr *)&servaddr, sizeof(servaddr))< 0)
+	if ((s = socket(af, SOCK_STREAM, 0)) < 0)
 		return -1;
-	
+	bzero((char *)&servaddr, sizeof(servaddr));
+	servaddr.sin_family = af;
+	inet_pton(AF_INET, servip, &servaddr.sin_addr);
+	servaddr.sin_port = htons(port);
+	if (connect(s, (struct sockaddr *)&servaddr, sizeof(servaddr))< 0)
+		return -1;
 	return s;
 }
-
